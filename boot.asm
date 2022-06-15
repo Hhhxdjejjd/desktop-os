0ca5cd933c18c18b568a018006849ea8e18f0125
Hhhxdjejjd <99557090+Hhhxdjejjd@users.noreply.github.com>
2022-06-15 14:54:48 +0000

    iddkkd

--- boot.asm
+++ boot.asm
@@ -1,6 +1,20 @@
-    cli
+    mov ax, 0x07c0
+    mov ds, ax
+
+    mov si, msg
+    cld
+ch_loop:lodsb
+   or al, al
+   jz hang
+   mov ah, 0x0E
+   mov bh, 0
+   int 0x10
+   jmp ch_loop
+   
 hang:
     jmp hang
+
+msg db "hello world", 13, 10, 0
 
     times 510-($-$$) db 0
     db 0x55
