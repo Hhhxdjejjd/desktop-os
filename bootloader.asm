5316b6ad4fcebb1fcf3c4ecd2586833c07c71f13
Hhhxdjejjd <99557090+Hhhxdjejjd@users.noreply.github.com>
2022-06-15 16:51:36 +0000

    ekkeke

--- bootloader.asm
+++ bootloader.asm
@@ -8,19 +8,7 @@ call printstring
 
 jmp $
 
-printstring:
-    mov ah, 0x0e
-    .loop:
-    cmp [bx], byte 0
-    je .Exit
-       mov al, [bx]
-       int 0x10
-       inc bx
-       jmp .loop
-    .Exit:
-    ret
-teststring:
-    db "hi im michael", 0
+%include "print.asm"
 
 times 510-($-$$) db 0
 
---
+++ print.asm
@@ -0,0 +1,16 @@
+printstring:
+     mov ah, 0x0e
+     .loop:
+     cmp [bx], byte 0
+     je .Exit
+        mov al, [bx]
+        int 0x10
+        inc bx
+        jmp .loop
+     .Exit:
+
+     pop ax
+     pop bx
+     ret
+teststring:
+    db "hi im michael", 0
